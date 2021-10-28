d13aeac176522c9b8e6ccbfb84f7c5a3e3769534
Miguel Ibarra <ibarrarellano@gmail.com>
2021-10-28 18:05:26 +0200

    Add data exploration notebook

---
+++ src/plot_distribution.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# Function to plot the distributions of a series data_count
+def plot_distribution(counts):
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots(1, 1, figsize=(6,4))
+    sns.barplot(y=counts.values, x=counts.index, ax=ax)
+    sns.despine(left=True)
+    ax.set_ylabel("Frequeny")
+    ax.set_xlabel(counts.name)
+    ax.set_title("Distribution")
+    plt.show()
