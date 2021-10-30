f442bcfedfb8559ace6709a66bdce556bc48dfb9
Miguel Ibarra <ibarrarellano@gmail.com>
2021-10-30 15:41:22 +0200

    Add medicine data exploration

--- src/plot_distribution.py
+++ src/plot_distribution.py
@@ -5,10 +5,11 @@ import pandas as pd
 # Function to plot the distributions of a series data_count
 def plot_distribution(counts):
     sns.set_style("whitegrid")
-    fig, ax = plt.subplots(1, 1, figsize=(6,4))
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
     sns.barplot(y=counts.values, x=counts.index, ax=ax)
     sns.despine(left=True)
     ax.set_ylabel("Frequeny")
     ax.set_xlabel(counts.name)
     ax.set_title("Distribution")
+    plt.xticks(rotation=90, ha='right')
     plt.show()
